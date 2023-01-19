e2d352de50aa7c941262e33e33801d7a3a6092ae
Gayle Hoefer <gaylehoefer@gmail.com>
2023-01-18 21:45:01 -0700

    Reviewed variable types

--- src/main/java/com/example/crashcourse/CrashCourseApplication.java
+++ src/main/java/com/example/crashcourse/CrashCourseApplication.java
@@ -9,5 +9,25 @@ public class CrashCourseApplication {
     public static void main(String[] args) {
         SpringApplication.run(CrashCourseApplication.class, args);
     }
+    //variables
+    String firstName = "Anna";
+
+    //types
+        //primitive types
+        char aLetter = 'a'; // a single charater in single quotes
+        boolean isLoggedIn = false;
+        int aWholeNum = 42;
+        double aDecimalPointNum = 42.42424;
+
+        //non-primitive types
+//       // object types must use capital letter
+    String aNewName = "Bob";
+    Double aNewNum = 54.56;
+    Integer aNewWholeNum = 90;
+    Boolean isLoggedOut = true;
+
+    Double anotherNum = (double) aNewWholeNum;
+    //privacy
+
 
 }
---
+++ src/main/java/com/example/crashcourse/Person.java
@@ -0,0 +1,4 @@
+package com.example.crashcourse;
+
+public class Person {
+}
